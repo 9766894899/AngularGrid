26975d613e5e8cfe95a3ab3cd821abae99fbf226
Sandesh <yogesandesh@gmail.com>
2024-06-19 16:04:50 +0530

    mat tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,25 @@ import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { FormsModule } from '@angular/forms';
 import { Table1Component } from './table1/table1.component';
 import { Table2Component } from './table2/table2.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
+import { TabsComponent } from './tabs/tabs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Table1Component,
-    Table2Component
+    Table2Component,
+    TabsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule, // Add HttpClientModule to imports
     NgxDatatableModule, // Add NgxDatatableModule to imports
-    FormsModule
+    FormsModule, 
+    BrowserAnimationsModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
